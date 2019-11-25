bd4ef95df531d62dbdd67bb71ce3ee24b5b3c8fa
TeddyYin <ltboegod143@gmail.com>
2019-11-25 14:37:24 +0800

    add models and code first.
    add model list
    * DefaultLanguage
    * LanguageContant
    * LanguageList
    * User

---
+++ Data/dotnetCoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using dotnetCore.Models;
+
+namespace dotnetCore.Data
+{
+    public class dotnetCoreContext : DbContext
+    {
+        public dotnetCoreContext (DbContextOptions<dotnetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<dotnetCore.Models.User> User { get; set; }
+        public DbSet<dotnetCore.Models.DefaultLanguage> DefaultLanguage { get; set; }
+        public DbSet<dotnetCore.Models.LanguageContant> LanguageContant { get; set; }
+        public DbSet<dotnetCore.Models.LanguageList> LanguageList { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using dotnetCore.Data;
 
 namespace dotnetCore
 {
@@ -24,6 +26,12 @@ namespace dotnetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<dotnetCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("dotnetCoreContext")));
+
+            //services.AddDbContext<dotnetCoreContext>(options =>
+            //        options.UseMySQL(Configuration.GetConnectionString("dotnetCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
